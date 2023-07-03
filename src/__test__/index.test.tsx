6ccb2b272c86942cbc615a3b78231798a96c56d1
hiroto <tohhiro@gmail.com>
2023-07-03 21:05:37 +0900

    テストを追加

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 
+describe("Plus", () => {
+  test("初期画面のPlusマークの数", () => {
+    render(<App />);
+    const plusMark = screen.getAllByText("+");
+    expect(plusMark.length).toBe(2);
+  });
+  test("クリックしたあとのPlusマークの数", async () => {
+    render(<App />);
+    const user = userEvent.setup();
+    const buttonElement = screen.getByRole("button");
+    await waitFor(() => {
+      user.click(buttonElement);
+      const plusMark = screen.getAllByText("+");
+      expect(plusMark.length).toBe(1);
+    });
+  });
+});
 describe("ボタンのテスト", () => {
   test("初期のボタンの表示", () => {
     render(<App />);
