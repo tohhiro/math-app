31c7e59aef4ac9a84a810c73c2f3f3d67f358dfe
hiroto <tohhiro@gmail.com>
2023-07-29 17:14:44 +0900

    4桁の数値になるように改修、およびvrtのファイルを改修

--- src/container/Main/index.tsx
+++ src/container/Main/index.tsx
@@ -4,11 +4,19 @@ import { useState } from "react";
 import { Question } from "./Question";
 import { Button } from "./Button";
 import { AnswerLabel } from "./AnswerLabel";
+import { number } from "prop-types";
+
+const createQuestion = () => {
+  const fourDigitNumber = Math.floor(Math.random() * 10000)
+    .toString()
+    .padEnd(4, "0");
+  return Number(fourDigitNumber);
+};
 
 export const Main: React.FC = () => {
   const [qq, setQuestion] = useState([
-    { q: Math.floor(Math.random() * 9999) },
-    { q: Math.floor(Math.random() * 9999) },
+    { q: createQuestion() },
+    { q: createQuestion() },
   ]);
 
   const [answer, setAnswer] = useState<number | null>(null);
@@ -30,10 +38,7 @@ export const Main: React.FC = () => {
   };
 
   const resetQuestion = () => {
-    setQuestion([
-      { q: Math.floor(Math.random() * 10000) },
-      { q: Math.floor(Math.random() * 10000) },
-    ]);
+    setQuestion([{ q: createQuestion() }, { q: createQuestion() }]);
     setAnswer(null);
   };
 
