37e9e438ef05d7f6e222e079cd082ad7ee544add
hiroto <tohhiro@gmail.com>
2024-07-06 14:36:04 +0900

    UTの軽微なリファクタリング

--- src/components/Main/Timer/index.test.tsx
+++ src/components/Main/Timer/index.test.tsx
@@ -3,29 +3,28 @@ import '@testing-library/jest-dom/extend-expect';
 import { Timer } from '.';
 
 describe('Timer', () => {
+  const mockValues = {
+    durationInMs: 1000 * 10 * 60,
+    isStarting: false,
+    onOverTime: jest.fn(),
+  };
+
   beforeEach(() => {
     jest.useFakeTimers();
   });
+
   afterEach(() => {
     jest.useRealTimers();
   });
+
   test('1000*10*60を渡すと05:00:000が表示される', () => {
-    const mockValues = {
-      durationInMs: 1000 * 10 * 60,
-      isStarting: false,
-      onOverTime: jest.fn(),
-    };
     render(<Timer {...mockValues} />);
     const timerLabel = screen.getByTestId('timer');
     expect(timerLabel).toHaveTextContent('05:00:000');
   });
+
   test('1000*10*60を渡たし、1秒待つと4秒台で表示される', async () => {
-    const mockValues = {
-      durationInMs: 1000 * 10 * 60,
-      isStarting: true,
-      onOverTime: jest.fn(),
-    };
-    render(<Timer {...mockValues} />);
+    render(<Timer {...mockValues} isStarting />);
     jest.advanceTimersByTime(1000);
     await waitFor(() => {
       const timerLabel = screen.getByTestId('timer');
