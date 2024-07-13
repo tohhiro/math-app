d01ceb93bada8026ab60e2a7264e319d01a9b175
hiroto <tohhiro@gmail.com>
2024-07-13 16:25:48 +0900

    `PropsWithChildren`の実装箇所を短縮記法、`it`を`test`に修正

--- src/components/Main/PartsLayout/index.tsx
+++ src/components/Main/PartsLayout/index.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import classes from './index.module.css';
 
-export const PartsLayout: React.FC<
-  PropsWithChildren<{ children: React.ReactNode }>
-> = ({ children }) => <div className={classes.container}>{children}</div>;
+export const PartsLayout: React.FC<PropsWithChildren> = ({ children }) => (
+  <div className={classes.container}>{children}</div>
+);
