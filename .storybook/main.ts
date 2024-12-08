dd478e0a0ebd9c83fbce6611e0a83baabd97b10e
hiroto <tohhiro@gmail.com>
2024-12-08 15:13:59 +0900

    storybookのmain.jsに設定を追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,23 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-    "storycap",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    'storycap',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  staticDirs: ["../public"],
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
 };
 export default config;
