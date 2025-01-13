309669bc67876be3650a880402b5555125d83007
hiroto <tohhiro@gmail.com>
2025-01-13 11:55:34 +0900

    MainのUTにて、複数の`userEvent`があったため、`describe`でラップし1つにまとめる

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -2,106 +2,102 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Main } from '.';
 
-describe('Plus', () => {
+describe('Main', () => {
   const user = userEvent.setup();
-  test('初期画面の「＋」の文字列の数が1つ表示されている', () => {
-    render(<Main />);
+  describe('Plus', () => {
+    test('初期画面の「＋」の文字列の数が1つ表示されている', () => {
+      render(<Main />);
 
-    const plusMark = screen.getAllByText('+');
-    expect(plusMark).toHaveLength(1);
+      const plusMark = screen.getAllByText('+');
+      expect(plusMark).toHaveLength(1);
+    });
+
+    test('1回クリックしたあと、Plusマークの数が1つ表示されている', async () => {
+      render(<Main />);
+
+      const buttonElement = screen.getAllByRole('button')[0];
+      await user.click(buttonElement);
+
+      const plusMark = screen.getAllByText('+');
+      expect(plusMark).toHaveLength(1);
+    });
   });
 
-  test('1回クリックしたあと、Plusマークの数が1つ表示されている', async () => {
-    render(<Main />);
+  describe('ボタンのテスト', () => {
+    test('初期のボタンの表示は「Start」になっている', () => {
+      render(<Main />);
 
-    const buttonElement = screen.getAllByRole('button')[0];
-    await user.click(buttonElement);
+      const buttonElement = screen.getAllByRole('button')[0];
+      expect(buttonElement.textContent).toContain('Start');
+    });
 
-    const plusMark = screen.getAllByText('+');
-    expect(plusMark).toHaveLength(1);
-  });
-});
+    test('2回クリックしたあとのボタンの表示は「Reset」になっている', async () => {
+      render(<Main />);
 
-describe('ボタンのテスト', () => {
-  const user = userEvent.setup();
+      const buttonElement = screen.getAllByRole('button')[0];
+      const clickButton = async (button: HTMLElement, times: number) => {
+        for (let i = 0; i < times; i++) {
+          await user.click(button);
+        }
+      };
 
-  test('初期のボタンの表示は「Start」になっている', () => {
-    render(<Main />);
-
-    const buttonElement = screen.getAllByRole('button')[0];
-    expect(buttonElement.textContent).toContain('Start');
-  });
-
-  test('2回クリックしたあとのボタンの表示は「Reset」になっている', async () => {
-    render(<Main />);
-
-    const buttonElement = screen.getAllByRole('button')[0];
-    const clickButton = async (button: HTMLElement, times: number) => {
-      for (let i = 0; i < times; i++) {
-        await user.click(button);
-      }
-    };
-
-    await clickButton(buttonElement, 2);
-    expect(buttonElement.textContent).toContain('Reset');
-  });
-});
-
-describe('問題の生成', () => {
-  const user = userEvent.setup();
-
-  test('初期値の表示がd「----」になる', () => {
-    render(<Main />);
-
-    const defaultQuestion = '----';
-    const questionLeft = screen.getByTestId('questionLeft').textContent;
-    const questionRight = screen.getByTestId('questionRight').textContent;
-
-    expect(questionLeft).toBe(defaultQuestion);
-    expect(questionRight).toBe(defaultQuestion);
+      await clickButton(buttonElement, 2);
+      expect(buttonElement.textContent).toContain('Reset');
+    });
   });
 
-  test('問題の数値が4桁を生成する', async () => {
-    render(<Main />);
+  describe('問題の生成', () => {
+    test('初期値の表示がd「----」になる', () => {
+      render(<Main />);
 
-    const questionLeft = screen.getByTestId('questionLeft').textContent;
-    const questionRight = screen.getByTestId('questionRight').textContent;
+      const defaultQuestion = '----';
+      const questionLeft = screen.getByTestId('questionLeft').textContent;
+      const questionRight = screen.getByTestId('questionRight').textContent;
 
-    const buttonElement = screen.getAllByRole('button')[0];
-    await user.click(buttonElement);
-    expect(questionLeft).toHaveLength(4);
-    expect(questionRight).toHaveLength(4);
-  });
-});
+      expect(questionLeft).toBe(defaultQuestion);
+      expect(questionRight).toBe(defaultQuestion);
+    });
 
-describe('計算', () => {
-  const user = userEvent.setup();
+    test('問題の数値が4桁を生成する', async () => {
+      render(<Main />);
 
-  test('問題を取得し計算結果が一致する', async () => {
-    render(<Main />);
-    const buttonElement = screen.getAllByRole('button')[0];
-    await user.click(buttonElement);
+      const questionLeft = screen.getByTestId('questionLeft').textContent;
+      const questionRight = screen.getByTestId('questionRight').textContent;
 
-    const questionLeft = screen.getByTestId('questionLeft').textContent;
-    const questionRight = screen.getByTestId('questionRight').textContent;
-    const ans = Number(questionLeft) + Number(questionRight);
-
-    expect(Number(ans)).toBe(ans);
+      const buttonElement = screen.getAllByRole('button')[0];
+      await user.click(buttonElement);
+      expect(questionLeft).toHaveLength(4);
+      expect(questionRight).toHaveLength(4);
+    });
   });
 
-  test('問題を取得し、問題の桁数が4桁、且つ計算結果が一致する', async () => {
-    render(<Main />);
+  describe('計算', () => {
+    test('問題を取得し計算結果が一致する', async () => {
+      render(<Main />);
+      const buttonElement = screen.getAllByRole('button')[0];
+      await user.click(buttonElement);
 
-    const buttonElement = screen.getAllByRole('button')[0];
-    await user.click(buttonElement);
+      const questionLeft = screen.getByTestId('questionLeft').textContent;
+      const questionRight = screen.getByTestId('questionRight').textContent;
+      const ans = Number(questionLeft) + Number(questionRight);
 
-    const questionLeft = screen.getByTestId('questionLeft').textContent;
-    const questionRight = screen.getByTestId('questionRight').textContent;
-    const ans = Number(questionLeft) + Number(questionRight);
-    await user.click(buttonElement);
+      expect(Number(ans)).toBe(ans);
+    });
 
-    expect(questionLeft).toHaveLength(4);
-    expect(questionRight).toHaveLength(4);
-    expect(Number(ans)).toBe(ans);
+    test('問題を取得し、問題の桁数が4桁、且つ計算結果が一致する', async () => {
+      render(<Main />);
+
+      const buttonElement = screen.getAllByRole('button')[0];
+      await user.click(buttonElement);
+
+      const questionLeft = screen.getByTestId('questionLeft').textContent;
+      const questionRight = screen.getByTestId('questionRight').textContent;
+      const ans = Number(questionLeft) + Number(questionRight);
+      await user.click(buttonElement);
+
+      expect(questionLeft).toHaveLength(4);
+      expect(questionRight).toHaveLength(4);
+      expect(Number(ans)).toBe(ans);
+    });
   });
 });
